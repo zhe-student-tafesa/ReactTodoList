e827bd5341bb209f9139ec5e69b6779c79a7a3e9
tom_glb <517277381_glb@qq.com>
2021-11-23 10:28:38 +1030

    3rd commit， Code optimization

--- TodoItem.js
+++ TodoItem.js
@@ -3,31 +3,38 @@ import './App.css';
 import React from 'react';
 //Fragment
 import {Fragment} from 'react';
-class TodoItem extends React.Component{//定义 Todolist类 继承 了 Component  ， render函数必须有 返回值
+class TodoItem extends React.Component{ //定义 Todolist类 继承 了 Component  ， render函数必须有 返回值
     constructor(props){
         super(props);//调用父类的构造函数  inputValue:'',//保存输入  list[]//保存TODOlist
-        //变量 放在 state里面
+        //通过bind(this)对handleItemClick函数的指向做一个修改
+
         this.handleItemClick=this.handleItemClick.bind(this);/////1111111111111在构造函数 绑定this
 
 
     }
   render(){
+    const { content }=this.props;//{content} 即this.props.content
  
   //父组件通过 属性向子组件传递数据  //父组件通过 属性向子组件传递数据 AAAAAAA
     return (  
-    <Fragment>
+      <Fragment>
+       <div onClick={this.handleItemClick}>
+         {content}
+       </div>
+      </Fragment>
 
-      <div onClick={this.handleItemClick}>{this.props.content}</div>
-       </Fragment>
-
-      )
+    )
  
   }
+
+  //修改list[]中的数据，来实现删除组件
   handleItemClick(){//在子组件的单击响应函数里（先传过来），再调用父组件的deleteItem方法
     //this.handleItemClick
     //console.log(1);
-    console.log(this.props.index);//2222222已经 在构造函数 绑定this
-    this.props.deleteItem(this.props.index);
+    const {deleteItem,index } = this.props;///////////做结构赋值
+    //console.log(this.props.index);//2222222已经 在构造函数 绑定this
+    //this.props.deleteItem(this.props.index); //优化成下边的编码
+    deleteItem(index);
 
   }
 }
--- Todolist.js
+++ Todolist.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
-import './App.css';
+
 import React from 'react';
 import TodoItem from './TodoItem';
 //Fragment
 import {Fragment} from 'react';
+import './App.css';
 class Todolist extends React.Component{//定义 Todolist类 继承 了 Component  ， render函数必须有 返回值
     constructor(props){
         super(props);//调用父类的构造函数  inputValue:'',//保存输入  list[]//保存TODOlist
@@ -12,6 +13,9 @@ class Todolist extends React.Component{//定义 Todolist类 继承 了 Component
             inputValue:'',
             list:[]
         };
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleBtnClick=this.handleBtnClick.bind(this);
+        this.handleItemClick=this.handleItemClick.bind(this);
 
     }
   render(){
@@ -23,25 +27,13 @@ class Todolist extends React.Component{//定义 Todolist类 继承 了 Component
       <div>
         <input
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
         />
-        <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+        <button onClick={this.handleBtnClick}>提交</button>
       </div>
       <ul>
         
-            {this.state.list.map(
-                (item,index)=>{
-                    return <TodoItem key={index} index={index} content={item} 
-                               deleteItem={this.handleItemClick.bind(this)}
-                            />
-                    {/*return <li key={index}
-                                onClick={this.handleItemClick.bind(this,index)}
-                            >
-                                {item}
-                            </li>*/}
-                }
-                )
-            }
+            {this.getTodoItem() }
 
        
         
@@ -51,20 +43,54 @@ class Todolist extends React.Component{//定义 Todolist类 继承 了 Component
 
           )
   }
+  getTodoItem(){// 别忘加 return，调用时别忘加 （）
+   return this.state.list.map(
+                (item,index)=>{
+                    return <TodoItem key={index} index={index} content={item} 
+                               deleteItem={this.handleItemClick}
+                            />
+                   
+                }
+                )
+
+  }
   handleInputChange(e){
     //console.log(e.target.value);
     //console.log(this);//使用.bind(this)后，this变成 Todolist
-    this.setState({inputValue:e.target.value});//11111111111111输入 并设置变量inputValue
+    /////this.setState({inputValue:e.target.value});//11111111111111输入 并设置变量inputValue
+    //新版React 推荐这么写， setState接收一个函数，而不是 一个 对象,this.setState(  ()=>{}  );,函数必须返回一个对象
+    const value = e.target.value;
+    this.setState(()=>{//////////////////可以 简写为：this.setState(()=>( {inputValue:value}));
+        return {inputValue:value};
+    });
+
   }
     handleBtnClick(){
-      this.setState({list:[...this.state.list,this.state.inputValue]});//把list拆包，然后加上最新输入的数据
-      this.setState({inputValue:""});//清空 输入框
-
+        //第1版
+      // this.setState({list:[...this.state.list,this.state.inputValue]});//把list拆包，然后加上最新输入的数据
+      // this.setState({inputValue:""});//清空 输入框
+      //第2版
+      // this.setState(()=>( {
+      //   list:[...this.state.list,this.state.inputValue],
+      //   inputValue:""
+      // }));
+      // //第3版
+        this.setState((prevState)=>( {//prevState为 上一个状态，即修改前的状态，用prevState代替this.state
+        list:[...prevState.list,prevState.inputValue],
+        inputValue:""
+      }));
     }
-    handleItemClick(index){
-      const myList=this.state.list;//建立副本
-      myList.splice(index,1);// 从指定的索引，删除一个元素
-      this.setState({list:myList});//把删除 后的 数组赋值 给显示的数组
+
+
+    handleItemClick(index){//删除 li
+      // const myList=this.state.list;//建立副本
+      // myList.splice(index,1);// 从指定的索引，删除一个元素
+      //this.setState({list:myList});//把删除 后的 数组赋值 给显示的数组
+      this.setState((prevState)=>{ 
+        const myList=prevState.list;
+        myList.splice(index,1);
+        return {list:myList} 
+        });
 
 
     }
